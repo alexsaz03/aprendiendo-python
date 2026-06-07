69b9ebc68f476f4bbfba1a1bacfa54bebe3706af
alexsaz <alexsaz03@email.com>
2026-06-07 16:17:32 +0200

    Semana 2 - loops

---
+++ semana2.py
@@ -0,0 +1,25 @@
+cultivo=input('Cual es el nombre del cultivo? ').lower()
+dias=int(input('Cuantos dias se van a registrar? '))
+kilosrecogidos=[]
+for i in range(dias):
+    kilos=int(input('Cuantos kilos se recogieron el dia ' + str(i+1) + '? '))
+    kilosrecogidos.append(kilos)
+total=0
+for kilos in kilosrecogidos:
+    total+=kilos
+print(f'En total se recogieron {total} kilos de {cultivo}') 
+promedio=total/dias
+print(f'El promedio de kilos recogidos por dia es {promedio:.2f} kilos') 
+diamayor=kilosrecogidos[0]
+for i in range(len(kilosrecogidos)):
+    if kilosrecogidos[i]>diamayor:
+        diamayor=kilosrecogidos[i]
+print(f'El dia que se recogieron mas kilos fue el dia {kilosrecogidos.index(diamayor)+1} con {diamayor} kilos')
+if promedio > 100:
+    print('Buena temporada')
+elif promedio > 50:
+    print('Temporada normal')
+else:
+    print('Temporada baja')
+
+    
